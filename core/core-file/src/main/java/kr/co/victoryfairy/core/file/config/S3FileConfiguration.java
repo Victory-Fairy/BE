a64abdac7523baf03552a104af4c678b92853e9a
김건호 <128780883+kimgho@users.noreply.github.com>
2026-08-27 22:52:39 +0900

    fix: S3 HTTP 클라이언트 의존성 충돌 해결 (#14)

--- core/core-file/src/main/java/kr/co/victoryfairy/core/file/config/S3FileConfiguration.java
+++ core/core-file/src/main/java/kr/co/victoryfairy/core/file/config/S3FileConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -15,6 +16,7 @@ public class S3FileConfiguration {
     @Bean
     S3Client s3Client(FileProperties fileProperties) {
         return S3Client.builder()
+            .httpClientBuilder(UrlConnectionHttpClient.builder())
             .region(Region.of(fileProperties.getS3Region()))
             .credentialsProvider(DefaultCredentialsProvider.builder().build())
             .build();
