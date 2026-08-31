d3b7d345a4ba3abdc110fc729835d9e56728a00a
kimgho <kgh67811@naver.com>
2026-08-31 19:12:39 +0900

    refactor: move admin features into API

---
+++ core/core-api/src/main/java/kr/co/victoryfairy/core/api/admin/config/AdminSecurityConfiguration.java
@@ -0,0 +1,16 @@
+package kr.co.victoryfairy.core.api.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AdminSecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
