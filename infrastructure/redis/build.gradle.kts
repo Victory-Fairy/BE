0c2bd940e3a4c58cb374d6bf1404bba4483f1a81
kimgho <kgh67811@naver.com>
2026-09-01 10:06:26 +0900

    build: align integrations with Spring Boot 3.5

--- infrastructure/redis/build.gradle.kts
+++ infrastructure/redis/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-aop")
-    implementation("org.redisson:redisson-spring-boot-starter:3.27.0")
+    implementation("org.redisson:redisson:3.27.0")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
